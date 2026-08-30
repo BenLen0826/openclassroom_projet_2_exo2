ae41e476e8c91fc59d58d37af59367d754588eab
benlen <ben.len.etudes@gmail.com>
2026-08-30 03:11:35 +0200

    feat : Ajout service avec avec filtrage des composants page

---
+++ src/app/services/countries.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {CountryModel} from "../models/country.model";
+import {map, Observable, of, tap} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+  private olympicUrl = './assets/mock/olympic.json';
+  private dataCountries: CountryModel[] = [];
+
+  constructor(private http:HttpClient) { }
+
+
+  getCountries(): Observable<CountryModel[]> {
+    if (this.dataCountries.length > 0) {
+      return of(this.dataCountries);
+    }
+
+    return this.http.get<CountryModel[]>(this.olympicUrl).pipe(
+      tap((data) => (this.dataCountries = data))
+    );
+  }
+
+  getCountryByName(countryName: string): Observable<CountryModel | undefined> {
+    return this.getCountries().pipe(
+      map((countries) => countries.find((c) => c.country === countryName))
+    );
+  }
+
+  getTotalCountries(countries: CountryModel[]): number {
+    return countries.length;
+  }
+
+  getTotalJOs(countries: CountryModel[]): number {
+    return new Set(
+      countries.flatMap((c) => c.participations.map((p) => p.year))
+    ).size;
+  }
+
+  getTotalMedalsForCountry(country: CountryModel): number {
+    return country.participations.reduce((sum, p) => sum + p.medalsCount, 0);
+  }
+
+  getTotalAthletesForCountry(country: CountryModel): number {
+    return country.participations.reduce((sum, p) => sum + p.athleteCount, 0);
+  }
+
+  getMedalsPerCountry(countries: CountryModel[]): number[] {
+    return countries.map((c) => this.getTotalMedalsForCountry(c));
+  }
+
+}
