44c0128ed12ca78d2b4802e2669d2a4990d1e4d1
benlen <ben.len.etudes@gmail.com>
2026-08-30 03:10:25 +0200

    feat : implémentation nouveau cpt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CountryComponent } from "./pages/country/country.component";
+import {CardComponent} from "./components/card/card.component";
+import {ChartComponent} from "./components/chart/chart.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {PageTitleComponent} from "./components/page-title/page-title.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, CountryComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CardComponent, ChartComponent, HeaderComponent, PageTitleComponent],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
