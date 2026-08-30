7956fc4a2c2817550cf57412f8e2e5ee519dbc86
benlen <ben.len.etudes@gmail.com>
2026-08-30 10:13:52 +0200

    refactor : modification des components home et country

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,7 +1,7 @@
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import Chart from 'chart.js/auto';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CountriesService} from "../../services/countries.service";
+import {CountryModel} from "../../models/country.model";
 
 
 @Component({
@@ -10,7 +10,7 @@ import Chart from 'chart.js/auto';
   styleUrls: ['./country.component.scss']
 })
 export class CountryComponent implements OnInit {
-  private olympicUrl = './assets/mock/olympic.json';
+  /*// private olympicUrl = './assets/mock/olympic.json';
   public lineChart!: Chart<"line", string[], number>;
   public titlePage: string = '';
   public totalEntries: any = 0;
@@ -63,5 +63,39 @@ export class CountryComponent implements OnInit {
       }
     });
     this.lineChart = lineChart;
+  }*/
+
+  public titlePage = '';
+  public totalEntries = 0;
+  public totalMedals = 0;
+  public totalAthletes = 0;
+  public error = '';
+
+  public years: string[] = [];
+  public medalsPerYear: number[] = [];
+
+  constructor(private route: ActivatedRoute, private countriesService: CountriesService, private router: Router) {}
+
+  ngOnInit(): void {
+    const countryName = this.route.snapshot.paramMap.get('countryName') ?? '';
+
+    this.countriesService.getCountryByName(countryName).subscribe({
+      next: (country) => this.handleData(country),
+      error: () => (this.error = 'Une erreur est survenue lors du chargement des données.')
+    });
+  }
+
+  private handleData(country: CountryModel | undefined): void {
+    if (!country) {
+      this.router.navigate(['not-found']);
+      return;
+    }
+
+    this.titlePage = country.country;
+    this.totalEntries = country.participations.length;
+    this.years = country.participations.map((p) => p.year.toString());
+    this.medalsPerYear = country.participations.map((p) => p.medalsCount);
+    this.totalMedals = this.countriesService.getTotalMedalsForCountry(country);
+    this.totalAthletes = this.countriesService.getTotalAthletesForCountry(country);
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
-import Chart from 'chart.js/auto';
+import {CountriesService} from "../../services/countries.service";
+import {CountryModel} from "../../models/country.model";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +9,8 @@ import Chart from 'chart.js/auto';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  private olympicUrl = './assets/mock/olympic.json';
+  /*
+  // private olympicUrl = './assets/mock/olympic.json';
   public pieChart!: Chart<"pie", number[], string>;
   public totalCountries: number = 0
   public totalJOs: number = 0
@@ -65,6 +66,36 @@ export class HomeComponent implements OnInit {
       }
     });
     this.pieChart = pieChart;
+  }*/
+
+  public totalCountries = 0;
+  public totalJOs = 0;
+  public error = '';
+
+  public countries: string[] = [];
+  public medalsPerCountry: number[] = [];
+
+  constructor(private router: Router, private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countriesService.getCountries().subscribe({
+      next: (data: CountryModel[]) => this.handleData(data),
+      error: () => (this.error = 'Une erreur est survenue lors du chargement des données.')
+    });
+  }
+
+  private handleData(data: CountryModel[]): void {
+    if (!data || data.length === 0) {
+      return;
+    }
+    this.countries = data.map((c) => c.country);
+    this.totalCountries = this.countriesService.getTotalCountries(data);
+    this.totalJOs = this.countriesService.getTotalJOs(data);
+    this.medalsPerCountry = this.countriesService.getMedalsPerCountry(data);
+  }
+
+  onCountryClick(countryName: string): void {
+    this.router.navigate(['country', countryName]);
   }
 }
 
